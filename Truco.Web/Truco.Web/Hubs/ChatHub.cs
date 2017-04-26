96415fddc4dac6e0efc2007d80342b746b489a95
gianstorani <gianstorani9@gmail.com>
2017-04-26 19:48:04 -0300

    Truco web

---
+++ Truco.Web/Truco.Web/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Truco.Web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void EnviarMensaje(string nombre, string texto)
+        {
+            Clients.All.mostrarMensaje(nombre, texto);
+        }
+        public void EnviarMensajePrivado(string nombre, string texto)
+        {
+            Clients.Caller.mostrarMensaje(nombre, texto);
+        }
+    }
+}
