ba955d963947c6f1cb5eccddd4275fedcef27563
gianstorani <gianstorani9@gmail.com>
2017-06-24 20:22:45 -0300

    Mano y Mazo

---
+++ TpTruco/TpTruco.Entidades/Mano.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TpTruco.Entidades
+{
+    public class Mano
+    {
+
+        public void Repartir()
+        {
+            List<Cartas> MazoCartas;
+        }
+
+        public void Mezclar()
+        {
+            var mazo = new Mazo();
+            var cartas = mazo.MazoCartas;
+            Shuffle(cartas);
+        }
+        private Random rng = new Random();
+
+        public void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
